9ca29721f76353c32b0755a8bc54312bcf3a4d70
João Carlos Rebouças <70778525+jcr04@users.noreply.github.com>
2022-09-13 19:27:45 -0300

    Update Controller-Reservas.js

--- reserva-sala-back/src/controlers/Controller-Reservas.js
+++ reserva-sala-back/src/controlers/Controller-Reservas.js
@@ -1,7 +1,7 @@
 'use strict'
 
-const mongoose = require('mongoose');
-const Reserva = mongoose.model('Reserva');
+const db = require('mongoose');
+const Reserva = db.model('Reserva');
 
 exports.get = (req, res, next) => {
     Reserva.find({})
